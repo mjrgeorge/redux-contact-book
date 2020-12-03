a552181fc82dd1c755bed364d90e44ab71222550
mjrgeorge <mjrgeorge@gmail.com>
2020-12-03 11:24:16 +0600

    Navbar and Table added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Contacts from './components/Contacts/Contacts';
+import Navbar from './components/Elements/Navbar';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container">
+        <div className="py-3">
+          <Contacts />
+        </div>
+      </div>
     </div>
   );
 }
